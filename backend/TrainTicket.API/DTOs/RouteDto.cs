8ca0445761e403f1e57465cf345a49731e0f350f
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-17 12:34:14 +0530

    feat: add RouteDto for representing route details in the API

---
+++ backend/TrainTicket.API/DTOs/RouteDto.cs
@@ -0,0 +1,8 @@
+namespace TrainTicket.API.DTOs;
+
+public class RouteDto
+{
+    public string DepartureStation { get; set; } = string.Empty;
+
+    public string DestinationStation { get; set; } = string.Empty;
+}
