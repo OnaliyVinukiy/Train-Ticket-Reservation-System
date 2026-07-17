586259e6a5f88017c2ec2064ada1496a70fb781a
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-17 22:58:55 +0530

    feat: implement request service for managing special requests

---
+++ frontend/src/services/requestService.ts
@@ -0,0 +1,36 @@
+import api from "./api";
+import type { SpecialRequest } from "../types/specialRequest";
+
+
+export const getRequests = async (): Promise<SpecialRequest[]> => {
+
+    const response = await api.get("/SpecialRequest");
+
+    return response.data;
+
+};
+
+
+export const createRequest = async (
+    request: SpecialRequest
+) => {
+
+    const response = await api.post(
+        "/SpecialRequest",
+        request
+    );
+
+    return response.data;
+
+};
+
+
+export const deleteRequest = async (
+    id: number
+) => {
+
+    await api.delete(
+        `/SpecialRequest/${id}`
+    );
+
+};
