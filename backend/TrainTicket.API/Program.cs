da31e3fdebe98cab72acc07c35388a6165384ea0
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-17 20:19:21 +0530

    feat: initialize SeedData in Program.cs for repository data setup

--- backend/TrainTicket.API/Program.cs
+++ backend/TrainTicket.API/Program.cs
@@ -1,3 +1,4 @@
+using TrainTicket.API.Data;
 using TrainTicket.API.Models;
 using TrainTicket.API.Repositories;
 using TrainTicket.API.Services;
@@ -26,6 +27,15 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var bookingRepo = scope.ServiceProvider.GetRequiredService<IRepository<Booking>>();
+    var scheduleRepo = scope.ServiceProvider.GetRequiredService<IRepository<Schedule>>();
+    var requestRepo = scope.ServiceProvider.GetRequiredService<IRepository<SpecialRequest>>();
+
+    SeedData.Initialize(bookingRepo, scheduleRepo, requestRepo);
+}
 
 
 if (app.Environment.IsDevelopment())
