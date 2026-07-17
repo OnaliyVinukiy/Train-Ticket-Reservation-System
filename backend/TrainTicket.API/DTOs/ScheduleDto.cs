cd15da527cce50a7db96845c42761b4d31624883
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-17 22:33:19 +0530

    feat: add Id property to ScheduleDto for unique identification

--- backend/TrainTicket.API/DTOs/ScheduleDto.cs
+++ backend/TrainTicket.API/DTOs/ScheduleDto.cs
@@ -2,6 +2,8 @@ namespace TrainTicket.API.DTOs;
 
 public class ScheduleDto
 {
+    public int Id { get; set; }
+
     public DateTime TravelDate { get; set; }
 
     public TimeSpan DepartureTime { get; set; }
