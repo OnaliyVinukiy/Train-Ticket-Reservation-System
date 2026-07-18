69c61408832639a9fb93cfbe0a1cf6cee4b6997d
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-18 20:28:11 +0530

    refactor: simplify GetById and Add methods in MemoryRepository

--- backend/TrainTicket.API/Repositories/MemoryRepository.cs
+++ backend/TrainTicket.API/Repositories/MemoryRepository.cs
@@ -1,54 +1,44 @@
+using System.Linq.Expressions;
+
 namespace TrainTicket.API.Repositories;
 
-public class MemoryRepository<T> : IRepository<T>
+public class MemoryRepository<T> : IRepository<T> where T : class
 {
     private readonly List<T> items = new();
     private int nextId = 1;
-
 
     public IEnumerable<T> GetAll()
     {
         return items;
     }
 
-
     public T? GetById(int id)
     {
-        return items
-            .Where(x =>
-                x!.GetType()
-                .GetProperty("Id")?
-                .GetValue(x)?
-                .Equals(id) == true)
-            .FirstOrDefault();
+        return items.FirstOrDefault(x =>
+        {
+            var property = x.GetType().GetProperty("Id");
+            if (property == null)
+                return false;
+            var value = property.GetValue(x);
+            return value != null && (int)value == id;
+        });
     }
-
 
     public void Add(T entity)
     {
-        var property = entity!
-            .GetType()
-            .GetProperty("Id");
-
+        var property = entity.GetType().GetProperty("Id");
         if (property != null)
-        {
             property.SetValue(entity, nextId++);
-        }
-
         items.Add(entity);
     }
 
-
     public void Update(T entity)
     {
-        var existing = GetById(
-            (int)entity!
-            .GetType()
-            .GetProperty("Id")!
-            .GetValue(entity)!
-        );
-
-
+        var property = entity.GetType().GetProperty("Id");
+        if (property == null)
+            return;
+        var id = (int)property.GetValue(entity)!;
+        var existing = GetById(id);
         if (existing != null)
         {
             items.Remove(existing);
@@ -56,14 +46,15 @@ public class MemoryRepository<T> : IRepository<T>
         }
     }
 
-
     public void Delete(int id)
     {
         var item = GetById(id);
-
         if (item != null)
-        {
             items.Remove(item);
-        }
+    }
+
+    public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+    {
+        return items.AsQueryable().Where(predicate).ToList();
     }
 }
