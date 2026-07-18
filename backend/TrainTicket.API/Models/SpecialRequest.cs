7dcad0e463d8ef8e527976a448e4f0c840d562d4
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-18 20:24:53 +0530

    refactor: update Description property initialization in SpecialRequest model

--- backend/TrainTicket.API/Models/SpecialRequest.cs
+++ backend/TrainTicket.API/Models/SpecialRequest.cs
@@ -1,8 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace TrainTicket.API.Models;
 
 public class SpecialRequest
 {
     public int Id { get; set; }
 
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } = "";
+
+
+    public int BookingId { get; set; }
+
+
+    [JsonIgnore]
+    public Booking? Booking { get; set; }
 }
