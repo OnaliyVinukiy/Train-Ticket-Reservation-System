257f7c4f2563c1489bf4c162df4dcfe7923dec52
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-18 20:58:25 +0530

    refactor: streamline GetWeeklyReport method in ReportController

--- backend/TrainTicket.API/Controllers/ReportController.cs
+++ backend/TrainTicket.API/Controllers/ReportController.cs
@@ -3,29 +3,22 @@ using TrainTicket.API.Services;
 
 namespace TrainTicket.API.Controllers;
 
-
 [ApiController]
 [Route("api/[controller]")]
 public class ReportController : ControllerBase
 {
-
     private readonly ReportService service;
 
-    public ReportController(
-        ReportService service)
+    public ReportController(ReportService service)
     {
         this.service = service;
     }
 
 
     [HttpGet("weekly")]
-    public IActionResult WeeklyReport()
+    public IActionResult GetWeeklyReport(DateTime date)
     {
-
-        return Ok(
-            service.GenerateWeeklyReport()
-        );
-
+        var result = service.GetWeeklyReport(date);
+        return Ok(result);
     }
-
 }
