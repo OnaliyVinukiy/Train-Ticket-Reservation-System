ba6ad0529d3b27eb0ae1bbe8d453a784825b245d
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-07-19 12:08:02 +0530

    feat: add ChatIntent enum to define various chat intents

---
+++ backend/TrainTicket.API/Models/ChatIntent.cs
@@ -0,0 +1,13 @@
+namespace TrainTicket.API.Models;
+
+public enum ChatIntent
+{
+    Greeting,
+    Help,
+    Availability,
+    PriceTrend,
+    Recommendation,
+    WeeklySummary,
+    PopularRoute,
+    Unknown
+}
