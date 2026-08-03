d563bbbef0ca215f0dbdb6d239ea4939cdf03b02
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-03 14:06:26 +0530

    feat: add SpecialRequest model for handling special requests in the reporting service

---
+++ backend/ReportingService.API/Models/SpecialRequest.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+
+namespace ReportingService.API.Models;
+
+public class SpecialRequest
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; } = "";
+
+
+    public int BookingId { get; set; }
+
+
+    [JsonIgnore]
+    public Booking? Booking { get; set; }
+}
