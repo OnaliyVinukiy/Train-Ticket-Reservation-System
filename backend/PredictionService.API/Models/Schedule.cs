220968227c878592ee28910a0d9cd743903e2b6d
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-03 16:09:37 +0530

    feat: add Schedule model for managing travel schedules

---
+++ backend/PredictionService.API/Models/Schedule.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace PredictionService.API.Models;
+
+public class Schedule
+{
+    public int Id { get; set; }
+
+    public DateTime TravelDate { get; set; }
+
+    public TimeSpan DepartureTime { get; set; }
+
+    public TimeSpan ArrivalTime { get; set; }
+
+}
