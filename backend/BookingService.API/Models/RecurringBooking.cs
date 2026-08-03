14f762ef45d0a6bca4bcf2e5f342a13a0ec4843e
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-03 20:56:55 +0530

    fix: change RecurrenceEndDate to nullable DateTime in RecurringBooking model

--- backend/BookingService.API/Models/RecurringBooking.cs
+++ backend/BookingService.API/Models/RecurringBooking.cs
@@ -5,7 +5,7 @@ public class RecurringBooking : Booking
     public RecurrencePattern RecurrencePattern { get; set; }
 
 
-    public DateTime RecurrenceEndDate { get; set; }
+    public DateTime? RecurrenceEndDate { get; set; }
     public RecurringBooking()
     {
         BookingType = BookingType.Recurring;
