e0cf3702479367ca6299e8d0f3d122692d79c892
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-03 20:57:18 +0530

    refactor: remove RecurrencePattern and RecurrenceEndDate properties from RecurringBooking model

--- backend/BookingService.API/Models/RecurringBooking.cs
+++ backend/BookingService.API/Models/RecurringBooking.cs
@@ -2,10 +2,6 @@ namespace BookingService.API.Models;
 
 public class RecurringBooking : Booking
 {
-    public RecurrencePattern RecurrencePattern { get; set; }
-
-
-    public DateTime? RecurrenceEndDate { get; set; }
     public RecurringBooking()
     {
         BookingType = BookingType.Recurring;
