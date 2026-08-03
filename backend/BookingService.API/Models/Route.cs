bc9dc31895622806ff453887ece32efa59969a69
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-03 17:50:47 +0530

    feat: add Route model to represent routes in the booking system

---
+++ backend/BookingService.API/Models/Route.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+namespace BookingService.API.Models;
+
+public class Route
+{
+    public int Id { get; set; }
+
+    public string DepartureStation { get; set; } = "";
+
+    public string DestinationStation { get; set; } = "";
+
+
+    [JsonIgnore]
+    public List<Booking> Bookings { get; set; } = new();
+}
