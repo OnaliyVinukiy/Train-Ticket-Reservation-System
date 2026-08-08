072c6f479cfdb329e43b1e77965497d7f5db70b0
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-08 22:55:19 +0530

    refactor: clean up Program.cs for improved readability and organization

--- backend/BookingService.API/Program.cs
+++ backend/BookingService.API/Program.cs
@@ -2,29 +2,25 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 using BookingService.API.Data;
-using BookingService.API.Models;
-
 using BookingService.API.Repositories;
 using BookingService.API.Services;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowReact",
-        policy =>
-        {
-            policy
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
 });
 
 
-
-builder.Services.AddControllers()
+builder.Services
+    .AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(
@@ -39,7 +35,8 @@ builder.Services.AddControllers()
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    );
 });
 
 
@@ -52,39 +49,27 @@ builder.Services.AddScoped<SpecialRequestRepository>();
 
 // Services
 builder.Services.AddScoped<BookingManagementService>();
-
 builder.Services.AddScoped<ScheduleService>();
-
 builder.Services.AddScoped<SpecialRequestService>();
-
 builder.Services.AddScoped<RecurringBookingService>();
-
 builder.Services.AddScoped<ExportService>();
 
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-    DataSeeder.Seed(context);
-}
 
 app.UseCors("AllowReact");
 
-if (true)
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// API Controllers
 app.MapControllers();
 
 app.Run();
