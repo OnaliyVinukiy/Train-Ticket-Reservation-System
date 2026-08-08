9e9d4b2a4053e7067c8ccd150008914bce45b161
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2026-08-08 22:51:23 +0530

    fix: remove automatic database migration during application startup

--- backend/BookingService.API/Program.cs
+++ backend/BookingService.API/Program.cs
@@ -72,8 +72,6 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-    context.Database.Migrate();
-
     DataSeeder.Seed(context);
 }
 
